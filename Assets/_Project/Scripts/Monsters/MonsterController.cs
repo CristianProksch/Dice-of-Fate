86a76c7064b8a9ddc99df845021224c495361478
Twinf0x <bastian.7741@gmail.com>
2022-07-17 15:06:45 +0200

    Fixed monster pin removal

--- Assets/_Project/Scripts/Monsters/MonsterController.cs
+++ Assets/_Project/Scripts/Monsters/MonsterController.cs
@@ -95,6 +95,7 @@ public class MonsterController : MonoBehaviour, IDamageable, IPinOwner
             var type = _monsterPins[pin];
             neutralPins.Remove(pin);
             _monsterPins.Remove(pin);
+            _grid.RemovePin(pin);
 
             var replacement = _grid.PlacePin(pin.transform.position, _monsterPinPrefabs[type]);
             _monsterPins.Add(replacement, type);
